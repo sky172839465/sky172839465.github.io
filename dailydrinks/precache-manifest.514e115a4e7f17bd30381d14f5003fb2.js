ce5cb63bd17e29b85fad9b69f8d0f590b23015e1
Yusong Hsu <sky172839465@gmail.com>
2019-06-20 08:09:04 +0800

    Add dailydrink page

---
+++ dailydrinks/precache-manifest.514e115a4e7f17bd30381d14f5003fb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88477f4ec6d2cab14c8d7a9e31e3877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45f5925f6bc936c66350",
+    "url": "/static/css/1.18d368d7.chunk.css"
+  },
+  {
+    "revision": "04b95fce21de5cb21d0c",
+    "url": "/static/css/2.fcd2daec.chunk.css"
+  },
+  {
+    "revision": "6b8f1303a8075fd247ec",
+    "url": "/static/css/3.2545e3f0.chunk.css"
+  },
+  {
+    "revision": "81434af75e0b3da8f705",
+    "url": "/static/css/main.dc887c31.chunk.css"
+  },
+  {
+    "revision": "2c4a9fd9b5d8214a1e9e",
+    "url": "/static/js/0.b385abde.chunk.js"
+  },
+  {
+    "revision": "45f5925f6bc936c66350",
+    "url": "/static/js/1.6e432916.chunk.js"
+  },
+  {
+    "revision": "04b95fce21de5cb21d0c",
+    "url": "/static/js/2.04f8b1a1.chunk.js"
+  },
+  {
+    "revision": "6b8f1303a8075fd247ec",
+    "url": "/static/js/3.4dd29e47.chunk.js"
+  },
+  {
+    "revision": "38c5c1eb7aae2d1bd021",
+    "url": "/static/js/6.67e9e346.chunk.js"
+  },
+  {
+    "revision": "81434af75e0b3da8f705",
+    "url": "/static/js/main.f0d6e079.chunk.js"
+  },
+  {
+    "revision": "4f2ec8f86f05e094336c",
+    "url": "/static/js/runtime~main.9d0b5b30.js"
+  }
+]);
